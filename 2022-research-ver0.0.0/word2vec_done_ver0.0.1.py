ad604e94c47f8c4d694c5dc00940b6b242c0f105
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-07-02 17:21:23 +0900

    complete basic

--- 2022-research-ver0.0.0/word2vec_done_ver0.0.1.py
+++ 2022-research-ver0.0.0/word2vec_done_ver0.0.1.py
@@ -14,7 +14,7 @@ from functions.functions import *
     - 効率を考えて今回はグローバルに定義
 """
 training_data = SampleDataVer001.get(run=1)
-test_data = W2VFunction.get_test_gold_data_filter_run()
+gold_cases = W2VFunction.get_test_gold_data_filter_run()
 model = BaseW2V.load(sg=1, size=100, min_count=10, window=5, name="gold", run=1)
 
 def _test_0():
@@ -22,15 +22,30 @@ def _test_0():
 
 
 def _test_1():
-    pass
+    test_case = training_data[1]
+    print("test_case")
+    pprint.pprint(test_case)
+    test_case_vector_obj = Vector(contexts=test_case, model=model, size=100)
+    test_case_vector = test_case_vector_obj.vector
+    for gold_case in gold_cases:
+        gold_case_vector_obj = Vector(contexts=gold_case, model=model, size=100)
+        gold_case_vector = gold_case_vector_obj.vector
+        try:
+            result = dot(test_case_vector, gold_case_vector)/(norm(test_case_vector)*norm(gold_case_vector))
+        except Exception as e:
+            pritn(e)
+        else:
+            if result > 0.9:
+                print("result:", result, ", ", )
+                pprint.pprint(gold_case)
+                
+
 
 
 
 
 def main():    
-    pprint.pprint(training_data[0])
-    print()
-    pprint.pprint(training_data[1])
+    _test_1()
 
 
 
