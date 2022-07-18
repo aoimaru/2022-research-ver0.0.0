d556480f74d08abf06a93a2c49ef13749e590907
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-07-18 15:51:31 +0900

    あげれるかな？(笑)

--- 2022-research-ver0.0.0/word2vec_done_ver0.1.0.py
+++ 2022-research-ver0.0.0/word2vec_done_ver0.1.0.py
@@ -73,10 +73,14 @@ def main(args):
     # do(args, target="APT-GET_INSTALL_ver0.1.0")
     # do(args, target="GPG_KEY_ver0.0.0")
     # do(args, target="APK_ADD_USE_NO_CACHE_ver0.0.0")
-    do(args, target="APT-GET_INSTALL_ver0.1.1")
-    do(args, target="APT-GET_INSTALL_ver0.2.0")
-    do(args, target="APT-GET_INSTALL_ver0.3.0")
-    do(args, target="APT-GET_INSTALL_ver0.4.0")
+    # do(args, target="APT-GET_INSTALL_ver0.1.1")
+    # do(args, target="APT-GET_INSTALL_ver0.2.0")
+    # do(args, target="APT-GET_INSTALL_ver0.3.0")
+    # do(args, target="APT-GET_INSTALL_ver0.4.0")
+    do(args, target="APT-GET_INSTALL_ver0.2.1")
+    do(args, target="APT-GET_INSTALL_ver0.3.1")
+    do(args, target="APT-GET_INSTALL_ver0.4.1")
+    do(args, target="APT-GET_INSTALL_ver0.5.1")
     # done()
 
 
